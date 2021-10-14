08b518979981c4ecaca56c9a89ea9e14ac0106bc
Laís Rhianne <lays.rhianne@gmail.com>
2021-10-13 21:28:09 -0300

    projeto iniciado

---
+++ src/components/CardEmpresa.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-card-title class="primary white--text"
+      ><h2>{{ empresa }}</h2></v-card-title
+    >
+    <v-card-content>
+      <v-container class="pa-3">
+        <v-avatar
+          v-show="mostrarAvatar"
+          color="primary"
+          size="56"
+          class="mb-3"
+        ></v-avatar>
+        <p>Ótimo lugar para trabalhar. Gostei muito da experiência</p>
+      </v-container>
+    </v-card-content>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "CardEmpresa",
+  props: {
+    empresa: String,
+  },
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-container id="cards-container">
+      <CardEmpresa
+        color="white"
+        class="mx-3"
+        width="300"
+        v-for="empresa in teste"
+        :key="empresa"
+        :empresa="empresa"
+      >
+      </CardEmpresa>
+    </v-container>
+    <v-text-field v-model="texto" label="Regular"></v-text-field>
+    <v-btn @click="logarTexto">TESTE</v-btn>
+  </v-container>
+</template>
+
+<script>
+import CardEmpresa from "./CardEmpresa.vue";
+export default {
+  name: "HelloWorld",
+
+  components: {
+    CardEmpresa,
+  },
+
+  data: () => ({
+    teste: ["Facebook", "Amazon", "Google", "Nubank"],
+    mostrarAvatar: false,
+    texto: "BATATA",
+  }),
+
+  methods: {
+    logarTexto() {
+      console.log(this.texto);
+    },
+  },
+};
+</script>
+
+<style scoped>
+#cards-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
